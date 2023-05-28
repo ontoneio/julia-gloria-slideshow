61600019a49f9850974f4a99519248dacc840073
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2023-05-27 20:22:31 -0700

    Basic working presentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro'
+
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [
+    UnoCSS({
+      injectReset: true // or a path to the reset file
+    }),
+  ],
 });
